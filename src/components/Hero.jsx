e2ee6b8d13def4d1af88f9f50521e01a16583aff
Oluseye Michael Ekerin <53854472+Seye4@users.noreply.github.com>
2024-07-02 12:06:16 +0800

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
               <h1 className="text-4xl pb-5 text-center font-bold text-clr-text-pry">Hi, I'm Oluseye</h1>
               <div className="h-12 overflow-hidden">
                 <div className=" h-full animate-move">
-                  <h1 id="autoText" className='text-clr-text-dark h-12 flex items-center justify-center text-3xl text-center'>Front End Developer </h1>
+                  <h1 id="autoText" className='text-clr-text-dark h-12 flex items-center justify-center text-3xl text-center'>Full Stack  Engineer </h1>
                   <h1 id="autoText" className='text-clr-text-dark h-12 flex items-center justify-center text-3xl text-center'>VR Developer </h1>
                   <h1 id="autoText" className='text-clr-text-dark h-12 flex items-center justify-center text-3xl text-center'>AR Developer </h1>
                   <h1 id="autoText" className='text-clr-text-dark h-12 flex items-center justify-center text-3xl text-center'>Game Developer </h1>
@@ -19,7 +19,7 @@ const Hero = () => {
               </div>
               
                 
-              <p className="w-1/2 mx-auto py-4 text-center text-clr-text-grey">I am a dedicated programmer with experience working on Frontend project, Augmented Reality, Virtual Reality and Game Development.  want to use my programming skills and expertise to add value to the employer and contribute to successful projects. Looking to take on new challenges and use my coding abilities for Web/VR/AR development. </p>
+              <p className="w-1/2 mx-auto py-4 text-center text-clr-text-grey">Am a Full Stack  Engineer with expertise in HTML, CSS, JavaScript, React, Next Js, Angular, Express  and other third-party frameworks such as jQuery, React and database systems like MongoDB and MySQL. I want to use my programming skills and expertise to add value to the employer and contribute to successful projects. Am looking forward to taking on new challenges.  </p>
             </div>
             <div className="flex items-center gap-x-2.5 pb-16">
               <a href="https://github.com/Seye4" target="_blank" rel="noopener noreferrer"><FaLinkedin className="h-8 w-8 fill-clr-pry" /></a>
